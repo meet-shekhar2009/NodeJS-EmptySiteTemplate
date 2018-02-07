fa278011e8a810f5b0bebabfec9ec6b7992cbe18
meet-shekhar2009 <36239797+meet-shekhar2009@users.noreply.github.com>
2018-02-08 00:11:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+
+
 var http = require('http');
 
-http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
-    
-}).listen(process.env.PORT || 8080);
+var server = http.createServer(function(req, res) {
+res.writeHead(200, {"Content-Type": "text/html"});
+res.end('<p>Here is a paragraph of <strong>HTML</strong>!</p>');
+});
+server.listen(8080);
